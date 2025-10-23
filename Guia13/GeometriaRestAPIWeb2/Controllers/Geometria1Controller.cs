efe35c3aa583995add6b054b3ec00b5a69fdb044
Facundo Citera <facundojesus10@hotmail.com>
2025-10-23 15:16:40 -0300

    23/10

--- Guia13/GeometriaRestAPIWeb2/Controllers/Geometria1Controller.cs
+++ Guia13/GeometriaRestAPIWeb2/Controllers/Geometria1Controller.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GeometriaRestAPIWeb.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,18 +9,22 @@ namespace GeometriaRestAPIWeb.Controllers
     [ApiController]
     public class Geometria1Controller : ControllerBase
     {
+        static List<FiguraDTO> listaFiguras = new List<FiguraDTO> {
+            new FiguraDTO{Id=1,Tipo=1,Area=3},
+            new FiguraDTO{Id=2,Tipo=2,Area=3.3}
+        };
         // GET: api/<Geometria1Controller>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<FiguraDTO> Get()
         {
-            return new string[] { "value1", "value2"};
+            return listaFiguras;
         }
 
         // GET api/<Geometria1Controller>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public FiguraDTO Get(int id)
         {
-            return "value";
+            return (from f in listaFiguras where f.Id == id select f).FirstOrDefault();
         }
 
         // POST api/<Geometria1Controller>
---
+++ Guia13/GeometriaRestAPIWeb2/DTOs/FiguraDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace GeometriaRestAPIWeb.DTOs
+{
+    public class FiguraDTO
+    {
+        public int? Id { get; set; }
+        public int? Tipo { get; set; }
+        public double? Area { get; set; }
+        public double? Ancho { get; set; }
+        public double? Largo { get; set; }
+        public double? Radio {  get; set; }
+
+        public override string ToString()
+        {
+            return $"Id:{this.Id}, Tipo:{this.Tipo}, Area:{this.Area}";
+        }
+    }
+}
